f6927fc67473c65bd5bdf36f063f4a1d065206a5
wallingTACC <walling@tacc.utexas.edu>
2020-01-22 10:55:39 -0600

    Fixed dashboard to work with simulated_data.R

--- admissions-dashboard/regression_model.R
+++ admissions-dashboard/regression_model.R
@@ -1,10 +1,10 @@
 # Script to create the regression model for the app
 # End result should be a .Rdata file containing the model, which can be loaded into the app
 
-setwd('/home/walling/dev/TERC-Crews/CollAppProto/')
+#setwd('/home/walling/dev/TERC-Crews/CollAppProto/')
 source('./simulated_data.R')
 
-model <- glm(accepted~school+hs_grad_type+num_adv+num_dual_credit+gpa+sat+school:gpa+school:sat, data=reg_data)
+model <- glm(accepted~school_fct+hs_grad_type_fct+num_adv+num_dual_credit+gpa+act+school_fct:gpa+school_fct:act, data=reg_data)
 
 summary(model)
 
@@ -16,7 +16,7 @@ predictions <- predict(model, newdata=test_data, type='response')
 
 # Plot predictions
 library(ggplot2)
-ggplot(test_data, aes(x=school, y=predictions)) +
+ggplot(test_data, aes(x=school_fct, y=predictions)) +
   geom_bar(stat="identity") +
   ylim(0,1)
 
--- admissions-dashboard/simulated_data.R
+++ admissions-dashboard/simulated_data.R
@@ -3,7 +3,7 @@ set.seed(1)
 # Common Factors
 race_fct <- as.factor(c('White', 'Black', 'Hispanic', 'Asian', 'Other'))
 hs_fact <- as.factor(c('Austin High', 'Westlake', 'Bowie'))
-college_fct <- as.factor(c('UT Austin', 'Texas A&M', 'Texas Tech', 'Texas State', 'UT Brownsville', 'UT Tyler', 'Houston', 'Rice', 'UT Elpaso'))
+school_fct <- as.factor(c('UT Austin', 'Texas A&M', 'Texas Tech', 'Texas State', 'UT Brownsville', 'UT Tyler', 'Houston', 'Rice', 'UT Elpaso'))
 hs_grad_type_fct <- as.ordered(c('low', 'mid', 'high'))
 levels(hs_grad_type_fct) <- c('low', 'mid', 'high')
 college_grad_status_fct <- as.ordered(c('un-enrolled', 'dropout', 'enrolled', 'graduated'))
@@ -26,12 +26,12 @@ next_grad_status <- function(grad_status) {
   }
 }
 
-is_accepted <- function(hs_grad_type, gpa, sat, num_adv) {
+is_accepted <- function(hs_grad_type, gpa, act, num_adv) {
   # Function allows us to simulate data with basic heuristics
   # I.e. higher gpa gives higher chance of accepted
   prob <- (0.25 * (as.numeric(hs_grad_type)/3.0)) + 
           (0.25 * (gpa / 4.0)) + 
-          (0.25 * (sat / 1600.0)) +
+          (0.25 * (act / 36.0)) +
           (0.25 * (num_adv / 10.0))
   return(sample(0:1, prob=c(1-prob,prob), size=1))
 }
@@ -41,22 +41,22 @@ size = 2000
 
 # Data for regression modesl
 # We base some values on others, namely 'accepted' so we need to create those first
-hs_grad_type <- sample(hs_grad_type_fct, size=size, replace=T)
+hs_grad_type_fct <- sample(hs_grad_type_fct, size=size, replace=T)
 gpa <- runif(n=size, min=2.0, max=4.0)
-sat <- sample(800:1600, size=size, replace=T)
+act <- sample(10:36, size=size, replace=T)
 num_adv <- sample(0:10, size=size, replace=T)
 num_dual_credit <- sample(0:10, size=size, replace=T)
 accepted <- sapply(1:size, function(i){
-  is_accepted(hs_grad_type[i], gpa[i], sat[i], num_adv[i])
+  is_accepted(hs_grad_type_fct[i], gpa[i], act[i], num_adv[i])
 })
 reg_data <- data.frame(id=1:size, 
                       accepted=accepted, 
-                      school=sample(college_fct, size=size, replace=T),
-                      hs_grad_type=hs_grad_type,
+                      school_fct=sample(school_fct, size=size, replace=T),
+                      hs_grad_type_fct=hs_grad_type_fct,
                       num_adv=num_adv,
                       num_dual_credit=num_dual_credit,
                       gpa=gpa,
-                      sat=sat
+                      act=act
                       )
 
 # Data for cohort visualizations
